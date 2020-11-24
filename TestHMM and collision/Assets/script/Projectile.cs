d1b9dc0144a9f907a6a54f8d819541f51df8ea56
RomannEspurr <romannyvinec@gmail.com>
2020-11-24 17:06:10 +0100

    modified testhmm

---
+++ TestHMM and collision/Assets/script/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private Material trailMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trailMaterial = new Material(Shader.Find("Specular"));
+        trailMaterial.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other) {
+        Vector3 contact = other.ClosestPoint(transform.position);
+
+        if (Vector3.Distance(contact, transform.position) < 0.2f){
+            LeaveTrail(contact, 0.1f, trailMaterial);
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        Vector3 contact = other.ClosestPoint(transform.position);
+        
+        LeaveTrail(contact, 0.2f, trailMaterial);
+        
+    }
+
+    //Ca ça marche
+    private void LeaveTrail(Vector3 point, float scale, Material material)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = Vector3.one * scale;
+        sphere.transform.position = point;
+        sphere.transform.parent = transform.parent;
+        sphere.GetComponent<Collider>().enabled = false;
+        sphere.GetComponent<Renderer>().material = material;
+        Destroy(sphere, 10f);
+    }
+}
---
+++ TestHMM and collision/Assets/script/Surface.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Surface : MonoBehaviour
+{
+    private static int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator BreakSurface(){
+        MeshFilter mf = GetComponent<MeshFilter>();
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Mesh baseMesh = mf.mesh;
+
+        foreach (var meshID in Enumerable.Range(0,baseMesh.subMeshCount)){
+            int[] allPointIdsList = baseMesh.GetTriangles(meshID); // Tout les points, ordonnés par triangle
+            for (int triangleID = 0; triangleID < allPointIdsList.Length; triangleID += 3){
+                
+                Mesh newMesh = new Mesh();
+                
+                newMesh.vertices = baseMesh.vertices.Skip(allPointIdsList[triangleID]).Take(3).Select(ver => Vector3.Scale(ver, transform.localScale)).ToArray();
+                newMesh.normals = baseMesh.normals.Skip(allPointIdsList[triangleID]).Take(3).ToArray();
+                newMesh.uv = baseMesh.uv.Skip(allPointIdsList[triangleID]).Take(3).ToArray();
+
+                newMesh.triangles = new int[] { 0, 1, 2, 2, 1, 0}; //front face + back face
+
+                GameObject GO = new GameObject("Fragment Triangle " + (triangleID / 3));
+                GO.transform.position = transform.position;
+                if(triangleID % 2 == 0){
+                    GO.transform.rotation = Quaternion.Inverse(transform.rotation);
+                }else{
+                    GO.transform.rotation = transform.rotation;
+                }
+                GO.AddComponent<MeshRenderer>().material = mr.materials[meshID];
+                GO.AddComponent<MeshFilter>().mesh = newMesh;
+                GO.AddComponent<BoxCollider>();
+                GO.AddComponent<Rigidbody>().AddExplosionForce(100, transform.position, 30);
+
+                Destroy(GO, 5 + UnityEngine.Random.Range(0.0f, 5.0f));
+            }
+        }
+
+        mr.enabled = false;
+        Time.timeScale = 0.2f;  //pour ralentir la scène
+        yield return new WaitForSeconds(0.8f);
+        Time.timeScale = 1.0f;
+        Destroy(gameObject);
+
+    }
+
+
+    void OnTriggerEnter(Collider other){
+        if(counter == 0){
+            Vector3 impactPoint = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+            Debug.Log(impactPoint);
+            StartCoroutine(BreakSurface());
+            counter++;
+        }
+        
+    }
+}
