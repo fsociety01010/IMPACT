06b49c731c5bd8a7b240d9b58f7c88fdd24731d6
e1601866 <audurier.e1601866@etud.univ-ubs.fr>
2020-12-12 21:02:39 +0100

    documentation + réorganisation code

--- IMPACT scene 1/Assets/Surface.cs
+++ IMPACT scene 1/Assets/Surface.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/*
+    Classe définissant le comportement à l'impacte de l'objet impacté
+*/
 public class Surface : MonoBehaviour
 {
     private static int counter = 0;
@@ -11,8 +14,6 @@ public class Surface : MonoBehaviour
     public float elasticLimit;
     private Material trailMaterial;
 
-    //rajouter et réflechir informations d'épaisseur par rapport à angle impact
-    // Start is called before the first frame update
     void Start()
     {
         this.trailMaterial = new Material(Shader.Find("Specular"));
@@ -25,7 +26,12 @@ public class Surface : MonoBehaviour
         
     }
 
-    private float distance(Vector3 reference_, Vector3[] vertices_){
+    /// <summary>
+    /// Fourni un nombre qui se rapproche de 0 lorsque le point reference se trouve dans le triangle créé par les 3 points de vertices_.
+    /// Sert à trouver quel triangle est le plus susceptible d'intégrer un point donné.
+    /// NE DOIT PAS ÊTRE UTILISE POUR TROUVER UNE DISTANCE, LE CALCUL EST MATHEMATIQUEMENT INEXACTE (pour optimisation).
+    /// </summary>
+    private float distanceToTriangle(Vector3 reference_, Vector3[] vertices_){
         float getArea(float[] abc_){
             float s = abc_.Sum() /2;
             return s * abc_.Aggregate(0f, (acc,val) => acc + s-val);
@@ -44,11 +50,12 @@ public class Surface : MonoBehaviour
                 (vertices_[(index+1)%3] - reference_).sqrMagnitude
             }
         ));
-
         return Mathf.Abs(distanceArea - baseArea);
-
     }
 
+    /// <summary>
+    /// Permet de traduire les coordonnées d'un point en WORLD COORDINATES en coordonnées correspondant au repère newSpace_ (qui dans la majorité des cas devrait être orthogonal et normal)
+    /// </summary>
     private Vector3 transfomToNewSpace(Vector3 vertex_, Matrix4x4 newSpace_){
         Vector3 fromOrigin = vertex_ - (Vector3)newSpace_.GetColumn(3);
         Vector3 newX = Vector3.Scale((Vector3)newSpace_.GetColumn(0), fromOrigin);
@@ -57,6 +64,11 @@ public class Surface : MonoBehaviour
         return newX + newY + newZ;
     }
 
+    /// <summary>
+    /// Permet d'obtenir un repère orthogonal et normal ayant pour origine le point "projectilePosition_".
+    /// Le X de ce repère correspondant à la normale de surface du triangle sur lequel l'impact a été détecté.
+    /// A utiliser avec transfomToNewSpace.
+    /// </summary>
     private Matrix4x4 getLocalImpactSpace(Mesh mesh_, Vector3 projectilePosition_){
         Matrix4x4 localToWorld = this.transform.localToWorldMatrix;
         Matrix4x4 worldToLocal = this.transform.worldToLocalMatrix;
@@ -64,7 +76,7 @@ public class Surface : MonoBehaviour
 
         int closestTriangle = mesh_.triangles
             .Where((_,i) => i%3==0)
-            .OrderBy(pointer => this.distance(epicenterInLocal, mesh_.vertices.Skip(pointer).Take(3).ToArray()))
+            .OrderBy(pointer => this.distanceToTriangle(epicenterInLocal, mesh_.vertices.Skip(pointer).Take(3).ToArray()))
             .First();
 
         Vector3[] closestVertices = mesh_.vertices.Skip(closestTriangle).Take(3).Select(ver => localToWorld.MultiplyPoint3x4(ver)).ToArray();
@@ -88,17 +100,49 @@ public class Surface : MonoBehaviour
         );
     }
 
-    /*
-    TODO remplacer calcul en fonction de x,y,z global par calcul sur x,y,z avec origine = point de contact, z = inverseNormaleFaceDeContacte
-    */
-    IEnumerator BreakSurfaceV2(Vector3 epiCenter_){
+    /// <summary>
+    /// Subdivise la mesh de base plusieurs nouvelles mesh, les mesh vont des vertex existant vers le vertex correspondant au point d'impacte (+ le même point mais du côté opposé de la surface)
+    /// </summary>
+    private void BreakSurface(Vector3 localisedEpicenter_, Vector3[] impactSideVertices_, Vector3[] oppositeSideVertices_, int currentMeshID_, MeshRenderer mr_){
+        for (int vIndex = 0; vIndex < impactSideVertices_.Length; vIndex += 3){
+            
+            Mesh newMesh = new Mesh();
+
+            if(vIndex == 0){
+                newMesh.vertices = new Vector3[] {impactSideVertices_[vIndex+3], impactSideVertices_[vIndex], new Vector3(impactSideVertices_[0].x, localisedEpicenter_.y, localisedEpicenter_.z), oppositeSideVertices_[vIndex+3], oppositeSideVertices_[vIndex], new Vector3(oppositeSideVertices_[0].x, localisedEpicenter_.y, localisedEpicenter_.z)};              
+            }else{
+                if(vIndex+3 >= impactSideVertices_.Length){
+                    newMesh.vertices = new Vector3[] {impactSideVertices_[vIndex], impactSideVertices_[vIndex-3], new Vector3(impactSideVertices_[0].x, localisedEpicenter_.y, localisedEpicenter_.z), oppositeSideVertices_[vIndex], oppositeSideVertices_[vIndex-3], new Vector3(oppositeSideVertices_[0].x, localisedEpicenter_.y, localisedEpicenter_.z)};
+                }else{
+                    newMesh.vertices = new Vector3[] {impactSideVertices_[vIndex+3], impactSideVertices_[vIndex-3], new Vector3(impactSideVertices_[0].x, localisedEpicenter_.y, localisedEpicenter_.z), oppositeSideVertices_[vIndex+3], oppositeSideVertices_[vIndex-3], new Vector3(oppositeSideVertices_[0].x, localisedEpicenter_.y, localisedEpicenter_.z)};
+                }
+            }
+            
+            newMesh.triangles = new int[] { 0,1,2, 2,5,3, 3,0,2, 2,1,5, 5,1,4, 4,1,0, 0,3,4, 4,3,5};
+
+            GameObject GO = new GameObject("Fragment Triangle " + (vIndex / 3));
+            GO.transform.position = this.transform.position;
+            GO.transform.rotation = this.transform.rotation;
+            GO.AddComponent<MeshRenderer>().material = mr_.materials[currentMeshID_];
+            GO.AddComponent<MeshFilter>().mesh = newMesh;
+            GO.AddComponent<BoxCollider>();
+            GO.AddComponent<Rigidbody>().AddExplosionForce(100, this.transform.position, 30);
+
+            //Destroy(GO, 5 + UnityEngine.Random.Range(0.0f, 5.0f));
+        }
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Génere l'ensemble des déformations liées au point d'impacte
+    /// </summary>
+    private IEnumerator Impact(Vector3 epiCenter_){
         MeshFilter mf = GetComponent<MeshFilter>();
         MeshRenderer mr = GetComponent<MeshRenderer>();
         Mesh baseMesh = mf.mesh;
 
         Matrix4x4 localImpactSpace = this.getLocalImpactSpace(baseMesh, epiCenter_);
         Matrix4x4 worldToLocal = this.transform.worldToLocalMatrix;
-        
         this.debugMatrix(localImpactSpace);
 
         float getAngleFromEpiCenter(Vector3 vertex){
@@ -110,45 +154,21 @@ public class Surface : MonoBehaviour
         Vector3 localisedEpicenter = Vector3.Scale(worldToLocal.MultiplyPoint3x4(epiCenter_), this.transform.localScale);
         LeaveTrail(epiCenter_, 0.1f, this.trailMaterial);
 
-        foreach (var meshID in Enumerable.Range(0,baseMesh.subMeshCount)){            
+        foreach (var meshID in Enumerable.Range(0,baseMesh.subMeshCount)){
             IEnumerable<Vector3> objectVertices = baseMesh.vertices.OrderBy(getAngleFromEpiCenter);
-            Vector3[] frontVertices = objectVertices.Where(v => v.x < 0).Select(ver => Vector3.Scale(ver, this.transform.localScale)).ToArray();
-            Vector3[] backVertices = objectVertices.Where(v => v.x >= 0).Select(ver => Vector3.Scale(ver, this.transform.localScale)).ToArray();
-
-            for (int vIndex = 0; vIndex < frontVertices.Length; vIndex += 3){
-                
-                Mesh newMesh = new Mesh();
-
-                if(vIndex == 0){
-                    newMesh.vertices = new Vector3[] {frontVertices[vIndex+3], frontVertices[vIndex], new Vector3(frontVertices[0].x, localisedEpicenter.y, localisedEpicenter.z), backVertices[vIndex+3], backVertices[vIndex], new Vector3(backVertices[0].x, localisedEpicenter.y, localisedEpicenter.z)};              
-                }else{
-                    if(vIndex+3 >= frontVertices.Length){
-                        newMesh.vertices = new Vector3[] {frontVertices[vIndex], frontVertices[vIndex-3], new Vector3(frontVertices[0].x, localisedEpicenter.y, localisedEpicenter.z), backVertices[vIndex], backVertices[vIndex-3], new Vector3(backVertices[0].x, localisedEpicenter.y, localisedEpicenter.z)};
-                    }else{
-                        newMesh.vertices = new Vector3[] {frontVertices[vIndex+3], frontVertices[vIndex-3], new Vector3(frontVertices[0].x, localisedEpicenter.y, localisedEpicenter.z), backVertices[vIndex+3], backVertices[vIndex-3], new Vector3(backVertices[0].x, localisedEpicenter.y, localisedEpicenter.z)};
-                    }
-                }
-                
-                
-                newMesh.triangles = new int[] { 0,1,2, 2,5,3, 3,0,2, 2,1,5, 5,1,4, 4,1,0, 0,3,4, 4,3,5};
-                
-
-                GameObject GO = new GameObject("Fragment Triangle " + (vIndex / 3));
-                GO.transform.position = this.transform.position;
-                GO.transform.rotation = this.transform.rotation;
-                GO.AddComponent<MeshRenderer>().material = mr.materials[meshID];
-                GO.AddComponent<MeshFilter>().mesh = newMesh;
-                GO.AddComponent<BoxCollider>();
-                GO.AddComponent<Rigidbody>().AddExplosionForce(100, this.transform.position, 30);
-
-                Destroy(GO, 5 + UnityEngine.Random.Range(0.0f, 5.0f));
-            }
+            Vector3[] impactSideVertices = objectVertices.Where(v => v.x < 0).Select(ver => Vector3.Scale(ver, this.transform.localScale)).ToArray();
+            Vector3[] oppositeSideVertices = objectVertices.Where(v => v.x >= 0).Select(ver => Vector3.Scale(ver, this.transform.localScale)).ToArray();
+            
+            //TODO créations de points à la périphérie du points d'impacte
+            //TODO appliquer déformation sur ces points
+            BreakSurface(localisedEpicenter, impactSideVertices, oppositeSideVertices, meshID, mr);
         }
-        mr.enabled = false;
+
+        //mr.enabled = false;
         //Time.timeScale = 0.01f;  //pour ralentir la scène
         yield return new WaitForSeconds(0.8f);
         Time.timeScale = 1.0f;
-        Destroy(gameObject);
+        
     }
 
     void OnCollisionEnter(Collision col){
@@ -156,10 +176,13 @@ public class Surface : MonoBehaviour
             counter++;
             Vector3 impactPoint = col.GetContact(0).point;
             //Debug.Log(impactPoint);
-            StartCoroutine(this.BreakSurfaceV2(impactPoint));
+            StartCoroutine(this.Impact(impactPoint));
         }
     }
 
+    /// <summary>
+    /// Fonction utilitaire qui permet d'observer un repère
+    /// </summary>
     private void debugMatrix(Matrix4x4 matrix_){
         Vector3 origin = matrix_.GetColumn(3);
         Vector3 x = matrix_.GetColumn(0);
@@ -177,10 +200,12 @@ public class Surface : MonoBehaviour
             origin,
             origin + z, Color.blue, 5f
         );
-        //Debug.Log(x);
-        //Debug.Log(y);
-        //Debug.Log(z);
-        //Debug.Log(origin);
+        /*
+        Debug.Log(x);
+        Debug.Log(y);
+        Debug.Log(z);
+        Debug.Log(origin);
+        */
     }
 
     private void LeaveTrail(Vector3 point, float scale, Material material)
