ae456575160375967544173b313dab7c2028bfc6
RomannEspurr <romannyvinec@gmail.com>
2021-01-04 20:41:42 +0100

    glass pane plays sound depending on what touched it

---
+++ IMPACT_project/Assets/script/restartScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class restartScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
